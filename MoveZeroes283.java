59b2951cf0e2a6a78e5822ecc1bc8458cee0e916
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2023-11-15 21:26:37 +0530

    Update MoveZeroes283.java
    added problem statement in the comments

--- MoveZeroes283.java
+++ MoveZeroes283.java
@@ -1,5 +1,22 @@
-package litcode.Day21thOCT;
+/*
 
-public class MoveZeroes283 {
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
 
+*/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] sec = new int[nums.length];
+		int j = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				sec[j++] = nums[i];
+
+			}
+		}
+        for(int l= 0 ; l<nums.length;l++) {
+			nums[l] = sec[l];
+        }
+    }
 }
