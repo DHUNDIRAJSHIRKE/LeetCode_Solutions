08538dfa23145d17e1e8b71bbef843fd11b88690
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2023-11-15 21:22:34 +0530

    Update SmallestLetter744.java
    added problem statement in the comment

--- SmallestLetter744.java
+++ SmallestLetter744.java
@@ -1,5 +1,11 @@
-package com.cdac.Tester;
+/*
 
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target.
+There are at least two different characters in letters.
+Return the smallest character in letters that is lexicographically greater than target.
+If such a character does not exist, return the first character in letters.
+
+*/
 public class SmallestLetter744 {
 
 	public static void main(String[] args) {
