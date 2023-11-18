428df302e86de334888b1075d5d9fc4220636228
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2023-11-18 15:19:07 +0530

    Add files via upload

---
+++ Q_1342_NumberOfStepsToReachToZero/Solution.java
@@ -0,0 +1,21 @@
+package Q_1342_NumberOfStepsToReachToZero;
+
+/*Given an integer num, return the number of steps to reduce it to zero.
+ *In one step, if the current number is even, you have to divide it by 2,
+ *otherwise, you have to subtract 1 from it.
+ */
+
+
+class Solution {
+    public int numberOfSteps(int num) {
+        int c= 0;
+        while(num!=0){
+            if(num%2==0){
+                num/=2;
+            }else{
+                num-=1;
+            }
+            c+=1;
+        }return c;
+    }
+}
