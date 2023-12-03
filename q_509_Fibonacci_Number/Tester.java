bf01c78d7c966aef33d7dd2ac65acdd4d50454de
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2023-12-03 09:07:04 +0530

    Add files via upload

---
+++ q_509_Fibonacci_Number/Tester.java
@@ -0,0 +1,11 @@
+package q_509_Fibonacci_Number;
+import static q_509_Fibonacci_Number.Solution.fib;
+public class Tester {
+
+	public static void main(String[] args) {
+		int n = 12;
+		System.out.println(fib(n));
+
+	}
+
+}
