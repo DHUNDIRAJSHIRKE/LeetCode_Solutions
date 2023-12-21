78908a4370cdf2d070b68115013d64f34d86c5f6
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2023-12-21 08:24:24 +0530

    Add files via upload

---
+++ q_2057_SmallestIndexWithEqualValue/Tester.java
@@ -0,0 +1,19 @@
+package q_2057_SmallestIndexWithEqualValue;
+import static q_2057_SmallestIndexWithEqualValue.Solution.smallestEqual;
+public class Tester {
+
+	public static void main(String[] args) {
+		//Test 1
+//		int[] nums = {4,3,2,1};
+		
+		//Test 2
+//		int[] nums = {1,2,3,4,5,6,7,8,9,0};
+		
+		
+		//Test 3
+		int[] nums = {4,3,2,1};
+		
+		System.out.println(smallestEqual(nums));
+	}
+
+}
