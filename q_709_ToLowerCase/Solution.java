86a11ae84c06e6cebbe8df9d7fc56ba111dfd591
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2023-12-24 10:34:25 +0530

    Add files via upload

---
+++ q_709_ToLowerCase/Solution.java
@@ -0,0 +1,16 @@
+package q_709_ToLowerCase;
+/*Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+ * 
+ * 
+ * Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+ * 
+ * */
+class Solution {
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+        
+    }
+}
---
+++ q_709_ToLowerCase/Tester.java
@@ -0,0 +1,24 @@
+package q_709_ToLowerCase;
+import static q_709_ToLowerCase.Solution.toLowerCase;
+public class Tester {
+
+	public static void main(String[] args) {
+		
+		//Test case 1
+//		String s = "Hello";
+		
+		
+		//Test case 2
+//		String s = "here";
+		
+		
+		//Test case 3
+		String s = "LOVELY";
+		
+		System.out.println(toLowerCase(s));
+		
+		
+		
+	}
+
+}
