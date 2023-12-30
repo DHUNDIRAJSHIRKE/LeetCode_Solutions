9752c3c16b0fc7b4dd029081e9c9a08071a6d55d
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2023-12-30 21:49:50 +0530

    Add files via upload

---
+++ q_2154_KeepMultiplyingFoundValuesByTwo/Tester.java
@@ -0,0 +1,19 @@
+package q_2154_KeepMultiplyingFoundValuesByTwo;
+
+import static q_2154_KeepMultiplyingFoundValuesByTwo.Solution.findFinalValue;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// Test 1
+//		int[] num= {5,3,6,1,12};
+//		int original= 3;
+
+		// Test 2
+		int[] num = { 2,7,9};
+		int original = 4;
+
+		System.out.println(findFinalValue(num, original));
+	}
+
+}
