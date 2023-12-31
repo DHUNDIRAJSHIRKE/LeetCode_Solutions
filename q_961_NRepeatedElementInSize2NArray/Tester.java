1b8681dbbe2dfe87dd0503b7bc4bd80807dcd206
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2023-12-31 10:02:02 +0530

    Add files via upload

---
+++ q_961_NRepeatedElementInSize2NArray/Tester.java
@@ -0,0 +1,24 @@
+package q_961_NRepeatedElementInSize2NArray;
+
+import static q_961_NRepeatedElementInSize2NArray.Solution.repeatedNTimes;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// Test case 1
+//		int[] nums = {1,2,3,3};
+		// 3
+
+		// Test case 2
+//		int[] nums = { 2,1,2,5,3,2};
+		// 2
+
+		// Test case 3
+		int[] nums = { 5,1,5,2,5,3,5,4};
+		// 5
+
+		System.out.println(repeatedNTimes(nums));
+
+	}
+
+}
