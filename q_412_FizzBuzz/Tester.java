da0f5200dbdfad8db7a3e91792ae5b6c3ecbd1c1
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2024-01-02 07:48:52 +0530

    Add files via upload

---
+++ q_412_FizzBuzz/Tester.java
@@ -0,0 +1,21 @@
+package q_412_FizzBuzz;
+
+import static q_412_FizzBuzz.Solution.fizzBuzz;;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// Test Case 1
+		int n = 3;
+
+		// Test Case 1
+		n = 5;
+
+		// Test Case 1
+		n = 15;
+
+		System.out.println(fizzBuzz(n));
+
+	}
+
+}
